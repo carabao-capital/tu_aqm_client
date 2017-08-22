3049c815b8591b363e84335eed08ca9312f467ac
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2017-08-22 22:16:02 +0800

    initialize

---
+++ spec/tu_aqm_client_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe TuAqmClient do
+  it "has a version number" do
+    expect(TuAqmClient::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
